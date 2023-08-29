aca5965ab774b28f7bf0a69f10f9629060e406dd
erick <erickdeyvsomk@gmail.com>
2023-08-29 11:05:34 -0300

    Adicionando processos de leitura e edição de dados

--- app/controllers/item.controller.js
+++ app/controllers/item.controller.js
@@ -1,5 +1,5 @@
 const db = require("../models");
-const Items = db.Items;
+const Item = db.Items;
 const Op = db.Sequelize.Op;
 
 exports.create = (req, res) => {
@@ -33,28 +33,79 @@ exports.findAll = (req, res) => {
     const name = req.body.name;
     var condition = name ? {name: { [Op.like]: `%${name}$` } } : null;
 
-    Items.findAll({ where: condition })
+    Item.findAll({ where: condition })
     .then(data => {
         res.send(data);
     })
     .catch(err => {
         res.status(500).send({
             message:
-              err.message || "Ocorreu um erro ao listar os itens."
+              err.message || "Algum erro aconteceu ao tentar pesquisar pelos itens."
         })
     })
 };
 
 exports.findOne = (req, res) => {
+    const id = req.params.id;
 
+    Item.findByPk(id)
+      .then(data => {
+          if(data) {
+              res.send(data);
+          } else {
+              res.status(404).send({
+                  message: `Não foi possível encontrar um item com o id=${id}.`
+              });
+          }
+      })
+      .catch(err => {
+          res.status(500).send({
+              message: "Ocorreu um erro ao tentar encontrar um item com o id=" + id
+          });
+      });
 };
 
 exports.update = (req, res) => {
+    const id = req.params.id;
 
+    Item.update(req.body, {
+        where: {id: id}
+    })
+      .then(num => {
+          if(num == 1) {
+              res.send({
+                  message: "O item foi atualizado de maneira bem sucedida."
+              });
+          } else {
+              res.send({
+                  message: `Não foi possível atualizar o item com o id=${id}.`
+              });
+          }
+      })  
 };
 
 exports.delete = (req, res) => {
+    const id = req.params.id;
 
+    Item.destroy({
+        where: {id: id}
+    })
+      .then(num => {
+          if (num == 1) {
+              res.send({
+                  message: "O item foi apagado com sucesso!"
+              });    
+          } else {
+              res.send({
+                  message: `Não foi possível apagar o item com o id=${id}.`
+              });
+          }
+      })
+      .catch(err => {
+          res.status(500).send({
+              message: "Ocorreu um erro ao tentar apagar o item com o id=" + id
+          });
+      });
 };
 
 exports.deleteAll = (req, res) => {
